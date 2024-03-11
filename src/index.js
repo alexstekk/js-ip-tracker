27a7bdcde2daa8e8bbd53dde51b28c9ea54a4008
Alexander Stekolshchikov <alexstekk@yandex.ru>
2024-03-11 08:41:36 +0300

    validate ip adress

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { validateIp } from './helpers';
 const ipInput = document.querySelector('.search-bar__input');
 const btn = document.querySelector('.search-bar__btn');
 
@@ -5,10 +6,11 @@ ipInput.addEventListener('keypress', handleKey);
 btn.addEventListener('click', getData);
 
 function getData() {
-	//validate
-	fetch(`https://ipgeolocation.abstractapi.com/v1/?api_key=72050c5b0fbd48bd82e0f7889d35579f&ip_address=${ipInput.value}`)
-		.then((response) => response.json())
-		.then(console.log);
+	if (validateIp(ipInput.value)) {
+		fetch(`https://ipgeolocation.abstractapi.com/v1/?api_key=72050c5b0fbd48bd82e0f7889d35579f&ip_address=${ipInput.value}`)
+			.then((response) => response.json())
+			.then(console.log);
+	}
 }
 
 function handleKey(e) {
